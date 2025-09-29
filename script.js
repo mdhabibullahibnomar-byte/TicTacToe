3afe452a6bcd44d9aed19751f4e473cd7fa4a981
mdhabibullahibnomar-byte <mdhabibullahibnomar@gmail.com>
2025-09-29 11:12:56 +0600

    add inital files

---
+++ script.js
@@ -0,0 +1,67 @@
+let boxes = document.querySelectorAll(".box");
+let reset = document.querySelector("#reset");
+let newGame = document.querySelector("#new");
+let win = document.querySelector("#win");
+let msg = document.querySelector(".msg")
+
+let turnO = true;
+
+const winPatterns =[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if(turnO){
+            box.innerText =  "O";
+            turnO = false;
+        }
+        else{
+            box.innerText = "X";
+        turnO = true;
+        }
+        box.disabled = true;
+        checkWinner();
+    })
+}
+)
+const resetGame =() =>{
+    turnO= true;
+    enableBoxes();
+    msg.classList.add("hide");
+}
+const enableBoxes =()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText = "";
+    }
+}
+const showWinner = (vectory) =>{
+       win.innerText = `Congratulations, Winner is ${vectory}`;
+      msg.classList.remove("hide"); 
+      boxes.forEach(box =>{
+        box.disabled = true;
+
+      })
+}
+const checkWinner = () => {
+    for(let pattern of winPatterns){
+     
+        let pos0 = boxes[pattern[0]].innerText;
+        let pos1 = boxes[pattern[1]].innerText;
+        let pos2 = boxes[pattern[2]].innerText;
+    if(pos0 !="" && pos1 !="" && pos2 !="" ){
+        if(pos0 === pos1&& pos1===pos2){
+            showWinner(pos0);
+        }
+    }
+    }
+}
+reset.addEventListener("click",resetGame);
+newGame.addEventListener("click",resetGame);
